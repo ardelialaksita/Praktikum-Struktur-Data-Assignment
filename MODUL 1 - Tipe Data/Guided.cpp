452fbd7270d62c1d570396196db0726b9a5d0c75
ara <157208713+ardelialaksita@users.noreply.github.com>
2024-03-11 12:24:52 +0700

    Update Guided 2 - Tipe Data Abstrak.cpp

--- MODUL 1 - Tipe Data/Guided.cpp
+++ MODUL 1 - Tipe Data/Guided.cpp
@@ -1,7 +1,38 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-int main() {
-    cout << "Ini adalah file code guided praktikan" << endl;
+// Struct
+struct Mahasiswa
+{
+    const char *name;
+    const char *address;
+    int age;
+};
+
+int main()
+{
+
+    // Menggunakan struct
+    struct Mahasiswa mhs1, mhs2;
+
+    // Mengisi nilai ke struct
+    mhs1.name = "Dian";
+    mhs1.address = "Mataram";
+    mhs1.age = 22;
+    mhs2.name = "Bambang";
+    mhs2.address = "Surabaya";
+    mhs2.age = 23;
+
+    // Mencetak isi struct
+    printf("## Mahasiswa 1 ##\n");
+    printf("Nama: %s\n", mhs1.name);
+    printf("Alamat: %s\n", mhs1.address);
+    printf("Umur: %d\n", mhs1.age);
+
+    printf("## Mahasiswa 2 ##\n");
+    printf("Nama: %s\n", mhs2.name);
+    printf("Alamat: %s\n", mhs2.address);
+    printf("Umur: %d\n", mhs2.age);
+    
     return 0;
+    
 }
