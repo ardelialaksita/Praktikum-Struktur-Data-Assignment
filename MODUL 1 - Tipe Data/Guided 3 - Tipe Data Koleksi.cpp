b3794981b57db1c7fe6f0a0e1c48db17548d6e14
ara <157208713+ardelialaksita@users.noreply.github.com>
2024-03-11 12:26:41 +0700

    Create Guided 3 - Tipe Data Koleksi.cpp

---
+++ MODUL 1 - Tipe Data/Guided 3 - Tipe Data Koleksi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // deklarasi dan inisialisasi array
+    int nilai[5];
+    nilai[0] = 23;
+    nilai[1] = 50;
+    nilai[2] = 34;
+    nilai[3] = 78;
+    nilai[4] = 90;
+
+    //mencetak array
+    cout << "Isi array pertama : " << nilai[0] << endl;
+    cout << "Isi array kedua : " << nilai[1] << endl;
+    cout << "Isi array ketiga : " << nilai[2] << endl;
+    cout << "Isi array keempat : " << nilai[3] << endl;
+    cout << "Isi array kelima : " << nilai[4] << endl;
+    return 0;
+}
