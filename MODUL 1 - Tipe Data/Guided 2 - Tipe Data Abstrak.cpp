8bdee89c9cfe0f8fde4c6ebfe1f905ab900cc116
Ardelia Laksita <laksitaardelia@gmail.com>
2024-03-11 12:09:58 +0700

    first commit

---
+++ MODUL 1 - Tipe Data/Guided 2 - Tipe Data Abstrak.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Struct
+struct Mahasiswa
+{
+    const char *name;
+    const char *address;
+    int age;
+};
+
+int main()
+{
+
+    // Menggunakan struct
+    struct Mahasiswa mhs1, mhs2;
+
+    // Mengisi nilai ke struct
+    mhs1.name = "Dian";
+    mhs1.address = "Mataram";
+    mhs1.age = 22;
+    mhs2.name = "Bambang";
+    mhs2.address = "Surabaya";
+    mhs2.age = 23;
+
+    // Mencetak isi struct
+    printf("## Mahasiswa 1 ##\n");
+    printf("Nama: %s\n", mhs1.name);
+    printf("Alamat: %s\n", mhs1.address);
+    printf("Umur: %d\n", mhs1.age);
+
+    printf("## Mahasiswa 2 ##\n");
+    printf("Nama: %s\n", mhs2.name);
+    printf("Alamat: %s\n", mhs2.address);
+    printf("Umur: %d\n", mhs2.age);
+    
+    return 0;
+    
+}
