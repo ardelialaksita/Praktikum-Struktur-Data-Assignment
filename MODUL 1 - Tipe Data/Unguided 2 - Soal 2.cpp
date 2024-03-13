b4c38350b76ff9ae8ee5c5c3eba8d81c4aca5c2a
ardelia rachma <157208713+ardelialaksita@users.noreply.github.com>
2024-03-13 17:37:07 +0700

    Update Unguided 2 - Soal 2.cpp

--- MODUL 1 - Tipe Data/Unguided 2 - Soal 2.cpp
+++ MODUL 1 - Tipe Data/Unguided 2 - Soal 2.cpp
@@ -1,32 +1,52 @@
-//CONTOH PROGRAM STRUCT
 #include <iostream>
 #include <string>
 using namespace std;
 
-//Struct
-struct Fruit {
+// Struct
+struct Fruit
+{
     string name;
     string colour;
     string taste;
 };
 
-int main() 
+class FruitClass
 {
-    struct Fruit fruit1, fruit2;
-    fruit1.name = "Durian";
-    fruit1.colour = "Kuning";
-    fruit1.taste = "Manis";
-    fruit2.name = "Lemon";
-    fruit2.colour = "Kuning";
-    fruit2.taste = "Asam";
+    string name;
+    string colour;
+    string taste;
+};
+
+class FruitClass1
+{
+public:
+    string name;
+    string colour;
+    string taste;
+};
+
+int main()
+{
+    Fruit frt;
+    FruitClass1 frt1;
+
+    frt.name = "Apple";
+    frt.colour = "Red";
+    frt.taste = "Sweet";
+
+    frt1.name = "Orange";
+    frt1.colour = "Orange";
+    frt1.taste = "Sour";
 
     cout << "===== BUAH 1 =====" << endl;
-    cout << "Nama Buah : " << fruit1.name << endl;
-    cout << "Warna Buah : " << fruit1.colour << endl;
-    cout << "Rasa Buah: " << fruit1.taste << endl;
-    cout << "===== BUAH 2 =====" << endl;
-    cout << "Nama Buah : " << fruit2.name << endl;
-    cout << "Warna Buah : " << fruit2.colour << endl;
-    cout << "Rasa Buah: " << fruit2.taste << endl;
+    cout << "Nama Buah : " << frt.name << endl;
+    cout << "Warna Buah : " << frt.colour << endl;
+    cout << "Rasa Buah: " << frt.taste << endl;
+
+    cout << "\n===== BUAH 2 =====" << endl;
+    cout << "Nama Buah : " << frt1.name << endl;
+    cout << "Warna Buah : " << frt1.colour << endl;
+    cout << "Rasa Buah: " << frt1.taste << endl;
+
     return 0;
 }
