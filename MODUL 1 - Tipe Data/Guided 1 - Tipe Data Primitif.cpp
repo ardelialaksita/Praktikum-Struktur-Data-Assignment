305e74366efe927af9cb36a828f27b5c61b57dbb
Ardelia Laksita <laksitaardelia@gmail.com>
2024-03-19 12:47:58 +0700

    first commit

--- MODUL 1 - Tipe Data/Guided 1 - Tipe Data Primitif.cpp
+++ MODUL 1 - Tipe Data/Guided 1 - Tipe Data Primitif.cpp
@@ -1,36 +1,43 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-
-int main() {
+// Main program
+int main()
+{
     char op;
     float num1, num2;
-    
-    cout << "Masukkan operator (+, -, *, /): ";
+    // It allows user to enter operator i.e. +, -, *, /
+    cout << "Masukkan operator: ";
     cin >> op;
-    
+    // It allow user to enter the operands
     cout << "Masukkan angka pertama: ";
     cin >> num1;
     cout << "Masukkan angka kedua: ";
     cin >> num2;
 
-    switch (op) {
+    //Switch statement begins
+    switch (op)
+    {
+        // If user enter +
         case '+':
-            cout << "Hasil Penjumlahan: " << num1 + num2;
-            break;
+        cout << num1 + num2;
+        break;
+        // If user enter -
         case '-':
-            cout << "Hasil Pengurangan: " << num1 - num2;
-            break;
+        cout << num1 - num2;
+        break;
+        // If user enter *
         case '*':
-            cout << "Hasil Perkalian: " << num1 * num2;
-            break;
+        cout << num1 * num2;
+        break;
+        // If user enter /
         case '/':
-            if (num2 != 0)
-                cout << "Hasil Pembagian: " << num1 / num2;
-            else
-                cout << "Error! Nol tidak boleh sebagai pembagi.";
-            break;
+        cout << num1 / num2;
+        break;
+
+        //If the operator is other than +, -, *, or /,
+        // error message will display
         default:
-            cout << "Error! Operator is not correct";
-    } 
+        cout << "Error! Operator is not correct";
+    } // Switch statement ends
     return 0;
 }
---
+++ MODUL 2 - Array/readme.md
@@ -0,0 +1,49 @@
+# <h1 align="center">Laporan Praktikum Modul Tipe Data</h1>
+<p align="center">Arvinanto Bahtiar</p>
+
+## Dasar Teori
+
+Berikan penjelasan teori terkait materi modul ini dengan bahasa anda sendiri serta susunan yang terstruktur per topiknya.
+
+## Guided 
+
+### 1. [Nama Topik]
+
+```C++
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "ini adalah file code guided praktikan" << endl;
+    return 0;
+}
+```
+Kode di atas digunakan untuk mencetak teks "ini adalah file code guided praktikan" ke layar menggunakan function cout untuk mengeksekusi nya.
+
+## Unguided 
+
+### 1. [Soal]
+
+```C++
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "ini adalah file code unguided praktikan" << endl;
+    return 0;
+}
+```
+#### Output:
+![240302_00h00m06s_screenshot](https://github.com/suxeno/Struktur-Data-Assignment/assets/111122086/6d1727a8-fb77-4ecf-81ff-5de9386686b7)
+
+Kode di atas digunakan untuk mencetak teks "ini adalah file code guided praktikan" ke layar menggunakan function cout untuk mengeksekusi nya.
+
+#### Full code Screenshot:
+![240309_10h21m35s_screenshot](https://github.com/suxeno/Struktur-Data-Assignment/assets/111122086/41e9641c-ad4e-4e50-9ca4-a0215e336b04)
+
+
+## Kesimpulan
+Ringkasan dan interpretasi pandangan kalia dari hasil praktikum dan pembelajaran yang didapat[1].
+
+## Referensi
+[1] I. Holm, Narrator, and J. Fullerton-Smith, Producer, How to Build a Human [DVD]. London: BBC; 2002.
