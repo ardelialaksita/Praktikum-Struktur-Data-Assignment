1b13aa20883a2cc10334c604a2a092fe77391804
Ardelia Laksita <laksitaardelia@gmail.com>
2024-05-06 13:38:17 +0700

    alstrukdat

---
+++ MODUL 8 - Queue/Guided 1.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+using namespace std;
+
+cons inr maksimalQueue = 5
+int front = 0;
+int back = 0;
+
+string queueTeller[5];
+
+bool isFull() {
+    if (back == maksimalQueue) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isEmpty() {
+    if (back == 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+    void enqueueAntrian(string data) {
+        if(isFull()) {
+            cout << "Antrian Penuh" << endl;
+        } else {
+            queueTeller[back] = data;
+            front++;
+            back++;
+        } else {
+            queueTeller[back] = data;
+            back++;
+            }
+        }
+    }
+
+void dequeueAntrian() {
+    if (isEmpty()) {
+        cout << "Antrian Kosong" << endl;
+    } else {
+        for (int i = 0; i < back; i++) {
+            queueTeller[i] = queueTeller[i+1];
+        }
+        back--;
+    }
+}
+
+int countQueue() {
+    return back;
+}
+
+void clearQueue() {
+    if (isEmpty()) {
+        cout << "Antrian kosong" << endl;
+    } else {
+        for (int i = 0; i < back; i++) {
+            queueTeller[i] = "";
+        }
+        back = 0;
+        front = 0;
+    }
+}
+
+void viewQueue() {
+    cout << "Data antrian teller: " << endl;
+    for (int i = 0; i < maksimalQueue; i++) {
+        if (queueTeller[i] != "") {
+            cout << i+1 << ". " << queueTeller[i] << endl;
+        } else {
+            cout << i + 1 << ". (kosong)" << endl;
+        }
+    }
+}
+
+int main() {
+    enqueueAntrian("Andi");
+    enqueueAntrian("Maya");
+    viewQueue();
+    cout << "Jumlah antrian = " << countQueue() << endl;
+    dequeueAntrian();
+    viewQueue();
+    cout << "Jumlah antrian = " << countQueue() << endl;
+    clearQueue();
+    viewQueue();
+    cout << "Jumlah antrian = " << countQueue() << endl;
+    return 0;
+}
