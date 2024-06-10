f34f4f2271aa1d9e32d9e7e49123dd8d831ae401
ardelia rachma <157208713+ardelialaksita@users.noreply.github.com>
2024-06-10 14:10:48 +0700

    posttestara

--- POST TEST/Soal 3.cpp
+++ POST TEST/Soal 3.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+// Ardelia Rachma Laksita (2311110051) S1SD-04 A
+
+// PROGRAM SORTING MENGGUNAKAN BUBBLE SORT
+
 int data[n] = {5,1,4,2,8};
 
 void bubblesort() {
