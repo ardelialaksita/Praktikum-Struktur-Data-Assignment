1a6f2e704af719c5dc5248cf767b66f59a2d91d0
ardelia rachma <157208713+ardelialaksita@users.noreply.github.com>
2024-06-10 14:09:07 +0700

    posttestara

---
+++ POST TEST/Soal 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int data[n] = {5,1,4,2,8};
+
+void bubblesort() {
+    int temp;
+    int i;
+    int j;
+    int n;
+
+    for (i = 0; i < n; i++) {
+        if(i > j) {
+            
+        }
+        else {
+            return;
+        }
+    }
+}
