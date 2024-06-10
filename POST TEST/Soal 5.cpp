2240e2cf63a8b4d0b8292e20fc767e498cc02d4f
ardelia rachma <157208713+ardelialaksita@users.noreply.github.com>
2024-06-10 14:19:26 +0700

    posttestara

---
+++ POST TEST/Soal 5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Ardelia Rachma Laksita (2311110051) S1SD-04 A
+
+// PROGRAM HASH TABLE
+
+int nums[4] = {2,7,1,15};
+int target = 9;
+int angka1;
+int angka2;
+
+int checkAngka() {
+    if (angka1 == angka2) {
+        angka1[nums] = angka2[nums]
+        return;
+    }
+    return -1;
+}
+
+int main() {
+    cout << "[" << angka << "]" << endl;
+    checkAngka();
+
+    return 0;
+}
