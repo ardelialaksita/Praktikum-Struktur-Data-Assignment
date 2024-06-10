b3421bcafcee110e72d40ba92f80145f39523b3f
ardelia rachma <157208713+ardelialaksita@users.noreply.github.com>
2024-06-10 13:16:26 +0700

    posttestara

---
+++ POST TEST/Soal 1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Ardelia Rachma Laksita (2311110051) S1SD-04 A
+
+// PROGRAM MENCARI SUATU ANGKA TERLETAK PADA INDEKS KE BERAPA
+
+int main() { 
+    int n = 6; 
+    int data[n] = {1,2,3,4,5,5};
+    int cari = 5; // angka yang dicari adalah 5
+    int ketemu;
+    int i; // untuk mengetahui indeks
+    
+    for (i = 0; i < n; i++) {
+        if (data[i] == cari) {
+            ketemu = true;
+        }
+    }
+
+    // Apabila angka ditemukan di dalam data
+    if(ketemu) {
+        cout << "Angka " << cari << " ditemukan pada indeks ke-" << i << endl;
+    } 
+    // Apabila angka tidak ditemukan di dalam data
+    else {
+        cout << "Angka " << cari << " tidak ditemukan pada data tersebut." << endl;
+    }
+    return 0;
+}
